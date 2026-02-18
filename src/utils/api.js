e34fb44f3efa7600f64d12c158371e8d3e949694
Andresouzanl <devals2022@gmail.com>
2026-02-18 12:41:42 -0300

    ajustando URL da API para produção

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Cria instância da API
 export const api = axios.create({
-  baseURL: "http://localhost:9000", 
+  baseURL: "https://api-produtos-8p05.onrender.com/", 
   headers: {
     'Content-Type': 'application/json',
   }
